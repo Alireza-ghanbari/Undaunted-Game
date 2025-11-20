afa925828f4825dc11156f0250dfe1e472fc8ebc
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2025-11-20 22:06:21 +0330

    phase 1: add cell.h and cell.cpp

--- UndauntedPhase1/Cell.cpp
+++ UndauntedPhase1/Cell.cpp
@@ -1,3 +1,8 @@
 #include "Cell.h"
 
-Cell::Cell() {}
+Cell::Cell() : m_type(0) {}
+
+Cell::Cell(const QString &id, int type)
+    : m_id(id), m_type(type)
+{
+}
--- UndauntedPhase1/Cell.h
+++ UndauntedPhase1/Cell.h
@@ -1,10 +1,20 @@
 #ifndef CELL_H
 #define CELL_H
 
+#include <QString>
+
 class Cell
 {
 public:
     Cell();
+    Cell(const QString &id, int type);
+
+    QString id() const { return m_id; }
+    int type() const { return m_type; }
+
+private:
+    QString m_id;   // مثل A01 یا B05
+    int m_type;     // 0 یا 1 یا 2
 };
 
-#endif // CELL_H
+#endif
